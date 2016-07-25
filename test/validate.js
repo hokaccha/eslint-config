08e7be74f7ba801f01efe2fd9da4794bbcbc74d8
Takeru Naito <takeru.naito@gmail.com>
2016-07-25 16:44:33 +0900

    Update the rules
    From our existing repository.

--- test/validate.js
+++ test/validate.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict"
 
-let validator = require('eslint/lib/config/config-validator');
-let index = require('../index');
+let validator = require("eslint/lib/config/config-validator")
+let index = require("../index")
 
-validator.validate(index, 'index.js');
-console.log('ok');
+validator.validate(index, "index.js")
+/* eslint-disable no-console */
+console.log("ok")
